140f7bafa18dd3740bb314e4244097c5e744b9f1
azzamsa <me@azzamsa.com>
2021-03-22 00:46:56 +0700

    perf: remove unused CSS classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,31 @@
-module.exports = {
-  plugins: [
-    require("tailwindcss")('tailwind.config.js'),
-    require("autoprefixer"),
-    require("postcss-typed-css-classes")({
-      generator: "rust",
-      output_filepath: "src/generated/css_classes.rs",
-      content: [
-        { path: ['src/**/*.rs'] }],
-    }),
-  ]
+const postcssRustHelpers = require("./scripts/postcss_rust_helpers");
+
+module.exports = ( ctx ) => {
+  // we want to filter out unused css classes in production mode
+  // NOTE: ctx.env is set in postcss-cli `--env` configs
+  const usedCssClasses =
+        ctx.env === "production"
+        ? postcssRustHelpers.getUsedCssClasses()
+        : null;
+
+  console.log("postcss ctx.env: " + ctx.env);
+  return {
+    plugins: [
+      require("tailwindcss")('tailwind.config.js'),
+      require("autoprefixer"),
+      require("postcss-typed-css-classes")({
+        output_filepath: "src/generated/css_classes.rs",
+        generator: "rust",
+        filter: class_ => {
+          if (ctx.env === "production") {
+            return usedCssClasses.has(
+              postcssRustHelpers.escapeClassName(class_)
+            );
+          } else {
+            return true;
+          }
+        }
+      }),
+    ]
+  };
 };
